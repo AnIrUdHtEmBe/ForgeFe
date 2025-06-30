315856cd35edc84fc229dc1c7e78fbce73110348
BHAV0207 <bhavyajain4274@gmail.com>
2025-06-30 14:58:42 +0530

    added cicking dates feature to navigate

--- src/views/ViewPlan/components/WeekPlanView.tsx
+++ src/views/ViewPlan/components/WeekPlanView.tsx
@@ -5,26 +5,13 @@ import "./styles/week-plan-view.css";
 interface WeekPlanViewProps {
   // sessions: t_session[];
   activeIndex: number;
+  setActiveIndex: (index: number) => void;
   weekStartToEndDates: string[];
 }
 
 const WeekPlanView = (props: WeekPlanViewProps) => {
   return (
     <div className="week-plan-view-container">
-      {/* {props.sessions.map((session, i) => {
-        return (
-          <div
-            key={i}
-            style={{
-              backgroundColor: props.activeIndex === i ? "lightblue" : "white",
-            }}
-            className="--plan"
-          >
-            <span className="--date">{getDate(session.scheduledDate)}</span>
-          </div>
-        );
-      })} */}
-
       {props.weekStartToEndDates.map((session, i) => {
         return (
           <div
@@ -33,6 +20,7 @@ const WeekPlanView = (props: WeekPlanViewProps) => {
               backgroundColor: props.activeIndex === i ? "lightblue" : "white",
             }}
             className="--plan"
+            onClick={() => props.setActiveIndex(i)}
           >
             <span className="--date">{TodaysDate(session)}</span>
           </div>
